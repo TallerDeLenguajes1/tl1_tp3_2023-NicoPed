9b74aefd57725e9940d223e71379c5af3326676c
NicoPed <nikopedraza10@gmail.com>
2023-04-07 17:57:44 -0300

    Punto 4 Terminado

--- tp3P4.c
+++ tp3P4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 char *TiposProductos[]={"Galletas","Snack","Cigarrillos","Caramelos","Bebidas"};
 
 struct{
@@ -18,27 +18,69 @@ Producto *Productos ;//El tamaño de este arreglo depende de la variable
                     // “CantidadProductosAPedir”
 }typedef Cliente;
 
-int main(){
 
-    int cantClientes;
+int precioProd(Producto prod);
+
+int main(){
+    char aux[60];
+    int cantClientes,indiceAuxTipoDeProducto;
     do
     {
+    fflush(stdin);
     printf("\nIngrese la cantidad de Clientes que visito: ");
     scanf("%d",&cantClientes);
     } while (cantClientes > 5 || cantClientes < 1 );
 
     Cliente *mtzClientes = (Cliente *) malloc(sizeof(Cliente)*cantClientes);
 
-    printf("\nA continuación ingrese los datos del cliente");
+    printf("\n===== A continuación ingrese los datos del cliente =====");
 
     for (int i = 0; i < cantClientes; i++)
-    {   
-    printf("\nIngrese el nombre del usuario: ");
-    gets(mtzClientes->NombreCliente);
-    mtzClientes->ClienteID = i;
-    mtzClientes->CantidadProductosAPedir= 1+rand()%5;
-    mtzClientes ++;    
+    {  
+        fflush(stdin); 
+        printf("\nIngrese el nombre del usuario: ");
+        gets(aux);
+        mtzClientes[i].NombreCliente = (char *) malloc( sizeof (char) * strlen(aux));
+        strcpy(mtzClientes[i].NombreCliente, aux);
+        mtzClientes[i].ClienteID = i;
+        mtzClientes[i].CantidadProductosAPedir= 1+rand()%5;
+        mtzClientes[i].Productos = (Producto *) malloc(sizeof(Producto) * mtzClientes[i].CantidadProductosAPedir);
+        for (int j = 0; j < mtzClientes[i].CantidadProductosAPedir; j++)
+        {
+          mtzClientes[i].Productos[j].ProductoID = j;
+          mtzClientes[i].Productos[j].Cantidad = 1 + rand() % 10;
+          indiceAuxTipoDeProducto = rand()%4;
+          mtzClientes[i].Productos[j].TipoProducto = (char *) malloc(sizeof(char) * strlen(TiposProductos[indiceAuxTipoDeProducto]));
+          strcpy(mtzClientes[i].Productos[j].TipoProducto,TiposProductos[indiceAuxTipoDeProducto]);
+          mtzClientes[i].Productos[j].PrecioUnitario = 10 + rand() % 91;
+          printf("PU: %d", mtzClientes[i].Productos[j].PrecioUnitario);
+        }
+        
     }
-
+    int totalPagarCliente=0;
+    for (int i = 0; i < cantClientes; i++)
+    {
+        totalPagarCliente = 0;
+        printf("\nNombre: %s",mtzClientes->NombreCliente);
+        printf("\nId: %d",mtzClientes->ClienteID);
+        printf("\nCant Prod: %d",mtzClientes->CantidadProductosAPedir);
+        printf("\n=== Detalle de los productos ===");
+        for (int j = 0; j < mtzClientes->CantidadProductosAPedir; j++)
+        {
+            printf("\nTipo de Producto: %s", mtzClientes->Productos->TipoProducto);
+            printf("\nProductoID: %d", mtzClientes->Productos->ProductoID);
+            printf("\nCantidad = %d",mtzClientes->Productos->Cantidad);
+            printf("\nPrecio Unitario= %.2f ",mtzClientes->Productos->PrecioUnitario);
+            totalPagarCliente += precioProd(mtzClientes->Productos[j]);
+            mtzClientes->Productos ++;
+        }
+        printf("\nTotal a Pagar del cliente Nro %d : %d", i, totalPagarCliente);
+        mtzClientes ++;
+    }
     return 0;
+}
+
+int precioProd(Producto prod){
+
+    return (prod.Cantidad * prod.PrecioUnitario);
 }
